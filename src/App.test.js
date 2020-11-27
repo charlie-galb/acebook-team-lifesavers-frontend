5a3a0f46fb09bd5fa6554b14a1c2d422d8d7fe19
Emily Sesto <emilyalicesesto@gmail.com>
2020-11-27 09:27:49 +0000

    registration test updated to exclude credentials as no longer required

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import { jest } from "@jest/globals";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Home/i);
-  expect(linkElement).toBeInTheDocument();
+let container = null;
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it('renders the home sign up/log in page', () => {
+  act(() => {
+    render(<App />, container);
+  });
+  expect(container.textContent).toMatch(/Sign Up/);
+  expect(container.textContent).toMatch(/Log In/);
 });
